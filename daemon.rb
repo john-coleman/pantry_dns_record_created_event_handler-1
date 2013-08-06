912d1c23eb31a024d195721b989054607b7ba18a
Clive Foley <clive.foley@wonga.com>
2013-08-06 11:46:25 +0100

    Adding in the config folder to the path

--- daemon.rb
+++ daemon.rb
@@ -6,7 +6,7 @@ require 'common/config'
 require_relative 'dns_record_created_event_handler'
 
 THIS_FILE = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-config = Daemons::Config.new(File.expand_path(File.join(File.dirname(THIS_FILE),"daemon.yml")))
+config = Daemons::Config.new(File.expand_path(File.join(File.dirname(THIS_FILE),"config","daemon.yml")))
 daemon_config = {
   :backtrace => config['daemon']['backtrace'],
   :dir_mode => config['daemon']['dir_mode'].to_sym,
