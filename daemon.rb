b891f4380f5e4b6e10a34a4532284891a3bb8902
John Coleman <john.coleman@wonga.com>
2013-08-14 12:58:18 +0100

    Fix class naming typo in daemon.rb

--- daemon.rb
+++ daemon.rb
@@ -4,4 +4,4 @@ require 'wonga/daemon'
 require_relative 'dns_record_created_event_handler/dns_record_created_event_handler'
 dir_name = File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__)
 Wonga::Daemon.load_config(File.expand_path(File.join(dir_name,"config","daemon.yml")))
-Wonga::Daemon.run(Wonga::Daemon::DNSRecordCreatedEventHandler.new(Wonga::Daemon.config, Wonga::Daemon.logger))
+Wonga::Daemon.run(Wonga::Daemon::DnsRecordCreatedEventHandler.new(Wonga::Daemon.config, Wonga::Daemon.logger))
