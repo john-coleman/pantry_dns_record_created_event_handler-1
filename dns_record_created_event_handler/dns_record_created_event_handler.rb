cab6fe7c98adb83f6e3a379cdcbc9fbe8925d949
Alex Slynko <alex.slynko@wonga.com>
2013-10-17 08:17:00 +0100

    Update to newer version of pantry_daemon_common

--- dns_record_created_event_handler/dns_record_created_event_handler.rb
+++ dns_record_created_event_handler/dns_record_created_event_handler.rb
@@ -9,7 +9,7 @@ module Wonga
 
       def handle_message(message)
         @logger.info "Updating booted status for Request:#{message["pantry_request_id"]}, Name:#{message["instance_name"]}, InstanceID:#{message["instance_id"]}"
-        response = @api_client.update_ec2_instance(message["pantry_request_id"], { joined: true, instance_id: message["instance_id"]})
+        @api_client.send_put_request("/api/ec2_instances/#{message["pantry_request_id"]}", { joined: true })
         @logger.info "Updating dns created status for Request:#{message["pantry_request_id"]} succeeded"
       end
     end
