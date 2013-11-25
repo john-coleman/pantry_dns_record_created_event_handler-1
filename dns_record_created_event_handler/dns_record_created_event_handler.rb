87d0003d72d15859867b6042cb467f928c66f8b6
Alex Slynko <alex.slynko@wonga.com>
2013-11-25 09:14:53 +0000

    Update event to support new StateMachine

--- dns_record_created_event_handler/dns_record_created_event_handler.rb
+++ dns_record_created_event_handler/dns_record_created_event_handler.rb
@@ -8,8 +8,8 @@ module Wonga
       end
 
       def handle_message(message)
-        @logger.info "Updating booted status for Request:#{message["pantry_request_id"]}, Name:#{message["instance_name"]}, InstanceID:#{message["instance_id"]}"
-        @api_client.send_put_request("/api/ec2_instances/#{message["pantry_request_id"]}", { joined: true })
+        @logger.info "Updating dns creating status for Request:#{message["pantry_request_id"]}, Name:#{message["instance_name"]}, InstanceID:#{message["instance_id"]}"
+        @api_client.send_put_request("/api/ec2_instances/#{message["pantry_request_id"]}", { event: :create_dns_record, joined: true, dns: true })
         @logger.info "Updating dns created status for Request:#{message["pantry_request_id"]} succeeded"
       end
     end
--- spec/dns_record_created_event_handler/dns_record_created_event_handler_spec.rb
+++ spec/dns_record_created_event_handler/dns_record_created_event_handler_spec.rb
@@ -8,7 +8,7 @@ describe Wonga::Daemon::DnsRecordCreatedEventHandler do
   it_behaves_like "handler"
 
   it "updates Pantry using PantryApiClient" do
-    expect(api_client).to receive(:send_put_request).with("/api/ec2_instances/40", { joined: true })
+    expect(api_client).to receive(:send_put_request).with("/api/ec2_instances/40", { event: :create_dns_record, joined: true, dns: true })
     subject.handle_message(message)
   end
 end
